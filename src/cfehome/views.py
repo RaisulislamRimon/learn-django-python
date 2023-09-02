76b8132a198666ffd3c18ab60a2248fc82c817cc
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-09-02 22:23:39 +0600

    first python django route setup from templates directory

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def home_page(*args, **kwargs):
-    return HttpResponse("<h1>Hello, Python & Django Home page</h1>")
+# def home_page(*args, **kwargs):
+#     return HttpResponse("<h1>Hello, Python & Django Home page</h1>")
+
+
+def home_page(request, *args, **kwargs):
+    return render(request, "index.html")
 
 
 def about_page(*args, **kwargs):
