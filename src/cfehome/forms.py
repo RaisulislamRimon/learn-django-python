48be2cd183386641a6bf5986b14454a2c01683f4
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-09-04 01:24:27 +0600

    add style in django form

--- src/cfehome/forms.py
+++ src/cfehome/forms.py
@@ -8,6 +8,21 @@ class LandingPageForm(forms.Form):
     email = forms.EmailField()
     email2 = forms.EmailField(label="confirm email")
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(self.fields)
+        for field in self.fields:
+            print(field)
+            default_css_class = "form-control"  # bootstrap class
+            new_attrs = {
+                "class": default_css_class,
+                "id": f"{field}",
+                "placeholder": f"Your {field}",
+            }
+            if field == "email2":
+                new_attrs["placeholder"] = "Confirm your email"
+            self.fields[field].widget.attrs.update(new_attrs)
+
     def clean(self):
         data = self.cleaned_data
         email = data.get("email")
