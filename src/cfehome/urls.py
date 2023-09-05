f454ac71f514fdece82d03f4dbc99b5a9479e4bc
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-09-05 11:38:30 +0600

    update

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
-    path('', views.home_page),
-    path('home/', views.home_page),
-    path('about/', views.about_page),
-    path('admin/', admin.site.urls),
+    path("", views.home_page),
+    path("home/", views.home_page),
+    path("about/", views.about_page),
+    path("admin/", admin.site.urls),
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+# Create your views here.
+
 from .forms import LandingPageForm
 
 
